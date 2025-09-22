99bdd056917f69dba638db45a9065b7cd4ce9031
nsriv531 <nsriv531@mtroyal.ca>
2025-09-22 11:02:30 -0600

    added two sum problem

---
+++ LC#5/README.md
@@ -0,0 +1,5 @@
+# Easy Two Sum Problem
+
+# Originally I had a check in mind using a for loop and if statement, instead I used a hashmap using the Map() function in Java Script.
+
+# September 21, 2025
---
+++ LC#5/Solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let map = new Map(); // stores number -> index
+
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+
+        if (map.has(complement)) {
+            console.log("triggered"); // pair has been found
+            return [map.get(complement), i];
+        }
+
+        map.set(nums[i], i); // store current number and index
+    }
+};
